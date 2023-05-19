a835db5f1d0348ad66e49309c64592ce82e3f868
Ivan Uzkikh <uzkih.ivan@gmail.com>
2023-05-19 23:56:23 +0200

    Add Brewfile dump and setup.sh script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install brew apps
+brew bundle install --file /Users/ivan/.local/share/chezmoi/Brewfile
+
+# Install and setup fish shell
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
