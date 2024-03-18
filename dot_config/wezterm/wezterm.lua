8c4be6345970e4fac3f9aff0ce95742f9b945e5b
Ivan Uzkikh <uzkih.ivan@gmail.com>
2024-03-18 16:21:11 +0100

    Add new directory for fish shell custom functions

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,47 @@
+local wezterm = require 'wezterm'
+
+-- -- config.color_scheme = 'catppuccin-macchiato'
+-- -- config.color_scheme = 'Catppuccin Mocha'
+-- -- config.color_scheme = 'Monokai (terminal.sexy)'
+-- -- config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
+-- -- config.color_scheme = 'Monokai (dark) (terminal.sexy)'
+-- -- config.color_scheme = 'Monokai Remastered'
+
+return {
+    font = wezterm.font({ family = "Iosevka Nerd Font Mono", weight = "Regular" }),
+    font_size = 16,
+    initial_rows = 40,
+    initial_cols = 150,
+    color_scheme = "DimmedMonokai",
+    keys = {
+        { key = "Enter", mods = "ALT", action = "DisableDefaultAssignment" }, {
+        key = "c",
+        mods = "SHIFT|ALT",
+        action = wezterm.action({
+            SplitHorizontal = { domain = "CurrentPaneDomain" }
+        })
+    }, {
+        key = "x",
+        mods = "SHIFT|ALT",
+        action = wezterm.action({
+            SplitVertical = { domain = "CurrentPaneDomain" }
+        })
+    }, {
+        key = "LeftArrow",
+        mods = "ALT|SHIFT",
+        action = wezterm.action({ ActivatePaneDirection = "Left" })
+    }, {
+        key = "RightArrow",
+        mods = "ALT|SHIFT",
+        action = wezterm.action({ ActivatePaneDirection = "Right" })
+    }, {
+        key = "UpArrow",
+        mods = "ALT|SHIFT",
+        action = wezterm.action({ ActivatePaneDirection = "Up" })
+    }, {
+        key = "DownArrow",
+        mods = "ALT|SHIFT",
+        action = wezterm.action({ ActivatePaneDirection = "Down" })
+    }
+    }
+}
--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
-#!/bin/sh 
+#!/bin/sh
 
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install chezmoi
+brew install chezmoi
 
 # Install brew apps
 brew bundle install --file /Users/ivan/.local/share/chezmoi/Brewfile
@@ -9,3 +12,6 @@ brew bundle install --file /Users/ivan/.local/share/chezmoi/Brewfile
 # Install and setup fish shell
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
+
+# Change Screenshot location
+defaults write com.apple.screencapture location ~/Desktop/Screenshots
