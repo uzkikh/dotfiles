9f51878f387f0a9a7aaa3116ad7ade5be2e8fc0e
Ivan Uzkikh <ivan@uzkikh.com>
2024-04-08 11:32:45 +0200

    Update wezterm themes list

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,18 +1,40 @@
 local wezterm = require 'wezterm'
 
--- -- config.color_scheme = 'catppuccin-macchiato'
--- -- config.color_scheme = 'Catppuccin Mocha'
--- -- config.color_scheme = 'Monokai (terminal.sexy)'
--- -- config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
--- -- config.color_scheme = 'Monokai (dark) (terminal.sexy)'
--- -- config.color_scheme = 'Monokai Remastered'
-
 return {
     font = wezterm.font({ family = "Iosevka Nerd Font Mono", weight = "Regular" }),
     font_size = 16,
     initial_rows = 40,
     initial_cols = 150,
-    color_scheme = "DimmedMonokai",
+
+-- Catpuccin themes
+-- color_scheme = "Catppuccin Mocha",
+-- color_scheme = "Catppuccin Macchiato",
+-- color_scheme = "Catppuccin Frappe",
+-- color_scheme = "Catppuccin Latte",
+
+-- Gruvbox themes
+-- color_scheme = "GruvboxDarkHard",
+-- color_scheme = "Gruvbox (Gogh)",
+-- color_scheme = "Gruvbox Dark (Gogh)",
+-- color_scheme = "Gruvbox dark, hard (base16)",
+   color_scheme = "Gruvbox dark, medium (base16)",
+-- color_scheme = "Gruvbox dark, pale (base16)",
+-- color_scheme = "Gruvbox dark, soft (base16)",
+-- color_scheme = "Gruvbox light, hard (base16)",
+-- color_scheme = "Gruvbox light, medium (base16)",
+-- color_scheme = "Gruvbox light, soft (base16)",
+-- color_scheme = "Gruvbox Material (Gogh)",
+-- color_scheme = "GruvboxDark",
+-- color_scheme = "GruvboxDarkHard",
+-- color_scheme = "GruvboxLight",
+
+-- Monokai themes
+-- color_scheme = "DimmedMonokai",
+-- color_scheme = "Monokai (terminal.sexy)",
+-- color_scheme = "Monokai Pro Ristretto (Gogh)",
+-- color_scheme = "Monokai (dark) (terminal.sexy)",
+-- color_scheme = "Monokai Remastered",
+
     keys = {
         { key = "Enter", mods = "ALT", action = "DisableDefaultAssignment" }, {
         key = "c",
